c66ca323f9ded61e43bc255c30b4fe1e4862f58a
devakshayy <devakshay916@gmail.com>
2025-05-30 02:26:54 +0530

    type.js and the fake server created

---
+++ schema/FakeData.js
@@ -0,0 +1,45 @@
+const UserList = [
+  {
+    id: 1,
+    name: "Akshay",
+    age: 23,
+    nationality: "Indian",
+  },
+  {
+    id: 2,
+    name: "John",
+    username: "john",
+    age: 20,
+    nationality: "CANADA",
+  },
+  {
+    id: 3,
+    name: "Pedro",
+    username: "PedroTech",
+    age: 20,
+    nationality: "BRAZIL",
+  },
+  {
+    id: 4,
+    name: "Sarah",
+    username: "cameron",
+    age: 25,
+    nationality: "INDIA",
+  },
+  {
+    id: 5,
+    name: "Rafe",
+    username: "rafe123",
+    age: 60,
+    nationality: "GERMANY",
+  },
+  {
+    id: 6,
+    name: "Kelly",
+    username: "kelly2019",
+    age: 5,
+    nationality: "CHILE",
+  },
+];
+
+module.exports = { UserList };
---
+++ schema/typeDefs.js
@@ -0,0 +1,14 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`{
+     type User{
+       id:ID!
+       name:String!
+       age:Int!
+       nationality:String!
+     }
+     type Query{
+        users:[Users]!
+     }
+}
+`
